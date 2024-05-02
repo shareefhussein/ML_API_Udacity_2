f3bf5742a9979bc225d0172b9b67e5516a7ded89
shareefhussein <56579327+shareefhussein@users.noreply.github.com>
2024-05-02 11:20:51 +0300

    Update test_process_encoder.py

--- tests/test_process_encoder.py
+++ tests/test_process_encoder.py
@@ -39,38 +39,3 @@ def trained_models():
     return model, encoder, label_binarizer
 
 
-def test_model_performance(dataset, trained_models):
-    """
-    Test model performance on
-    test data to ensure that accuracy, precision, recall,
-    and F1 score metrics are within acceptable ranges.
-    """
-    model, encoder, label_binarizer = trained_models
-
-    categorical_features = [
-        "workclass", "education", "marital-status", "occupation",
-        "relationship", "race", "sex", "native-country",
-    ]
-
-    # Processing test data
-    x_test, y_test, _, _ = process_data(
-        dataset,
-        categorical_features=categorical_features,
-        label="salary",
-        encoder=encoder,
-        label_binarizer=label_binarizer,
-        training=False
-    )
-
-    # Predicting test data
-    y_pred = model.predict(x_test)
-
-    # Computing model performance metrics
-    accuracy = accuracy_score(y_test, y_pred)
-    precision, recall, f1_score = compute_model_metrics(y_test, y_pred)
-
-    # Assertions to check if metrics are within the expected range
-    assert 0.0 <= accuracy <= 1.0, "Accuracy out of acceptable range."
-    assert 0.0 <= precision <= 1.0, "Precision out of acceptable range."
-    assert 0.0 <= recall <= 1.0, "Recall out of acceptable range."
-    assert 0.0 <= f1_score <= 1.0, "F1 score out of acceptable range."
