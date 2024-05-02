4062f0f0432a63be74a3de2e840b4ed9d7beb88c
shareefhussein <56579327+shareefhussein@users.noreply.github.com>
2024-05-02 10:57:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="ML_API_Udacity",
     version="0.0.1",
-    description="3rd course project for the Udacity Machine Learning DevOps Engineer nanodegree",
+    description="3rd course project for the Udacity \
+    Machine Learning DevOps Engineer nanodegree",
     author="Shareef Mansour"
 )
