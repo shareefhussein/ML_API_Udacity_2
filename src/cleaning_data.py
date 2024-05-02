2d8d83d0dda9af9a671133fb8e56ce65423a10ae
shareefhussein <56579327+shareefhussein@users.noreply.github.com>
2024-05-02 10:29:02 +0300

    Update cleaning_data.py

--- src/cleaning_data.py
+++ src/cleaning_data.py
@@ -4,21 +4,26 @@ Cleaning data code
 
 import pandas as pd
 
+
 def clean_data():
     """
-    Clean the dataset for use in model training. This includes stripping whitespace,
+    Clean the dataset for use in model training.
+    This includes stripping whitespace,
     replacing problematic values, and removing unwanted columns.
     """
     data_frame = pd.read_csv("./data/raw_data/census.csv")
     # Strip whitespace from column names
     data_frame.columns = data_frame.columns.str.strip()
-    # Strip whitespace from string data and handle missing values
-    data_frame = data_frame.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    # Strip whitespace from string
+    data_frame = data_frame.applymap(
+        lambda x: x.strip() if isinstance(x, str) else x
+    )
     data_frame.replace({'?': None}, inplace=True)
     data_frame.dropna(inplace=True)
 
     # Drop unwanted columns
-    columns_to_drop = ["fnlgt", "education-num", "capital-gain", "capital-loss"]
+    columns_to_drop = ["fnlgt", "education-num",
+                       "capital-gain", "capital-loss"]
     data_frame.drop(columns=columns_to_drop, inplace=True)
 
     print("Export cleaned data to CSV file.")
