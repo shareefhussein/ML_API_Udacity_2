4f3dd1982946849eed2e51262826b678ddabd6e9
shareefhussein <56579327+shareefhussein@users.noreply.github.com>
2024-05-02 10:36:17 +0300

    Update process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -3,28 +3,35 @@ Prepare data code
 """
 
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+from sklearn.preprocessing import\
+    LabelBinarizer, OneHotEncoder
 
-def process_data(data_frame, categorical_features=None, continuous_features=None,
-                 label=None, training=True, encoder=None, label_binarizer=None):
+
+def process_data(
+        data_frame,
+        categorical_features=None,
+        continuous_features=None,
+        label=None,
+        training=True,
+        encoder=None,
+        label_binarizer=None):
     """
     Process the data used in the machine learning pipeline.
 
     Parameters:
-    - data_frame (pd.DataFrame): Dataframe containing the features and label.
-    - categorical_features (list[str], optional): Names of the categorical features.
-    - continuous_features (list[str], optional): Names of the continuous features.
-    - label (str, optional): Name of the label column in `data_frame`. If None, returns an empty array for y.
+    - data_frame: Dataframe containing the features and label.
+    - categorical_features: Names of the categorical features.
+    - continuous_features: Names of the continuous features.
+    - label: Name of the label column in df.
     - training (bool): Indicates if training mode or inference/validation mode.
-    - encoder (OneHotEncoder, optional): Trained sklearn OneHotEncoder, used if training=False.
-    - label_binarizer (LabelBinarizer, optional): Trained sklearn LabelBinarizer, used if training=False.
+    - encoder: Trained sklearn OneHotEncoder, used if training=False.
+    - label_binarizer: Trained sklearn LabelBinarizer, used if training=False.
 
     Returns:
     - np.array: Processed data.
     - np.array: Processed labels if labeled=True, otherwise empty array.
-    - OneHotEncoder: Trained OneHotEncoder if training is True, otherwise returns the encoder passed in.
-    - LabelBinarizer: Trained LabelBinarizer if training is True, otherwise returns the binarizer passed in.
+    - OneHotEncoder: Trained OneHotEncoder.
+    - LabelBinarizer: Trained LabelBinarizer.
     """
     if categorical_features is None:
         categorical_features = []
@@ -48,7 +55,8 @@ def process_data(data_frame, categorical_features=None, continuous_features=None
 
     categorical_data = encoder.transform(data_frame[categorical_features])
     continuous_data = data_frame[continuous_features].values
-    processed_data = np.concatenate([continuous_data, categorical_data], axis=1)
+    processed_data = np.concatenate([continuous_data,
+                                     categorical_data], axis=1)
 
     if label is not None:
         labels = label_binarizer.transform(labels.reshape(-1, 1)).ravel()
